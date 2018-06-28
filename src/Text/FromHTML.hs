af5726c107fbacbc5b9c73b38a8a0a54a2c19143
Marek Suchánek <marek.suchanek@gmail.com>
2018-06-28 10:50:56 +0200

    Producing PDF by Pandoc & wkhtmltopdf

--- src/Text/FromHTML.hs
+++ src/Text/FromHTML.hs
@@ -1,7 +1,8 @@
-module Text.FromHTML
-    ( fromHTML
-    , ExportType(..)
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
+module Text.FromHTML where
+--    ( fromHTML
+--    , ExportType(..)
+--    ) where
 
 -- import Debug.Trace
 
@@ -12,18 +13,31 @@ import qualified Data.ByteString.Lazy as BL
 
 import qualified Text.Pandoc as Pandoc
 import qualified Text.Pandoc.Writers as PandocWriters
+import qualified Text.Pandoc.Error as PandocError
+import qualified Text.Pandoc.PDF as PandocPDF
 
+import           System.IO.Unsafe
 
 -- | Transform given HTML as String to selected format
 fromHTML :: ExportType -> String -> Maybe B.ByteString
 fromHTML HTML html = Just . E.encodeUtf8 . T.pack $ html  -- HTML is already provided!
-fromHTML extp html = case mpd of
-                        Just pd -> eitherToMaybe . Pandoc.runPure $ actwriter Pandoc.def pd
-                        _       -> Nothing
+fromHTML PDF html = runOnPD writerHTML2PDF (html2pd html)
+fromHTML extp html = runOnPD (\d pd -> Pandoc.runPure $ actwriter d pd) (html2pd html)
+  where actwriter = writer extp
+
+html2pd :: String -> Maybe Pandoc.Pandoc
+html2pd html = eitherToMaybe . Pandoc.runPure $ Pandoc.readHtml Pandoc.def (T.pack html)
+
+-- | Ugly PDF writer from HTML
+writerHTML2PDF opts pd = fixError . unsafePerformIO . Pandoc.runIO $ PandocPDF.makePDF "wkhtmltopdf" ["--quiet"] PandocWriters.writeHtml5String opts pd
   where
-    mpd :: Maybe Pandoc.Pandoc
-    mpd = eitherToMaybe . Pandoc.runPure $ Pandoc.readHtml Pandoc.def . T.pack $ html
-    actwriter = writer extp
+    fixError (Left pderr) = Left pderr
+    fixError (Right (Left bserr)) = Left . PandocError.PandocSomeError . T.unpack . E.decodeUtf8 . BL.toStrict $ bserr
+    fixError (Right (Right x)) = Right (BL.toStrict x)
+
+runOnPD f (Just pd) = eitherToMaybe (f Pandoc.def pd)
+runOnPD _ Nothing   = Nothing
+
 
 data ExportType = HTML
                 | LaTeX
@@ -37,6 +51,7 @@ data ExportType = HTML
                 | MediaWiki
                 | EPUB2
                 | EPUB3
+                | PDF
                 deriving (Show, Read, Enum, Bounded, Eq)
 
 type Writer = (Pandoc.WriterOptions -> Pandoc.Pandoc -> Pandoc.PandocPure B.ByteString)
@@ -60,6 +75,7 @@ writer = wrapWriter . pandocWriter
     wrapWriter (Pandoc.ByteStringWriter bsw) = \opts pd -> BL.toStrict <$> bsw opts pd
 
 
+-- | Pick Pandoc writer for pure transformation
 pandocWriter :: ExportType -> Pandoc.Writer Pandoc.PandocPure
 pandocWriter HTML      = Pandoc.TextWriter PandocWriters.writeHtml5String
 pandocWriter LaTeX     = Pandoc.TextWriter PandocWriters.writeLaTeX
@@ -73,3 +89,4 @@ pandocWriter Docx      = Pandoc.ByteStringWriter PandocWriters.writeDocx
 pandocWriter ODT       = Pandoc.ByteStringWriter PandocWriters.writeODT
 pandocWriter EPUB2     = Pandoc.ByteStringWriter PandocWriters.writeEPUB2
 pandocWriter EPUB3     = Pandoc.ByteStringWriter PandocWriters.writeEPUB3
+pandocWriter PDF       = pandocWriter HTML -- cannot be done as PandocPure
